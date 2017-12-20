47071f700abf59ba1bdd005c68d2c1814256fac8
neex-io <neex.io@protonmail.com>
2017-12-20 12:12:12 -0800

    Remove name dependency. Save players in a list of dicts

--- league_builder.py
+++ league_builder.py
@@ -2,27 +2,30 @@ import csv
 
 # import CSV data and save to a dictionary
 
-def csv_to_dictionary(csv_file):
+def csv_to_list_of_dicts(csv_file):
 
-  with open(csv_file, newline='') as players_csv:
-    player_reader = csv.reader(players_csv)
-    players_list = list(player_reader) # list of lists
-    players_list.pop(0) # remove description line 
+    with open(csv_file, newline='') as players_csv:
+        player_reader = csv.reader(players_csv)
+        players_list = list(player_reader) # list of lists
+        players_list.pop(0) # remove description line 
+        
+        '''players_list conatins each player is a list with this information order:
+        Name = player[0]
+        Height (in inches) = player[1]
+        Experience = player[2]
+        Guardian name(s) = player[3]'''
     
-    '''players_list conatins each player is a list with this information order:
-    Name = player[0]
-    Height (in inches) = player[1]
-    Experience = player[2]
-    Guardian name(s) = player[3]'''
-  
-    # create a dicitionary of players
-    # the script assumes that there are no players with the same name
-    player_dic = {}
+    # create a list with each player as dicitionary
+    list_player_dic = []
     for player in players_list:
-      player_information = [player[1], player[2], player[3]] # value for key:value pair of player data
-      player_dic[player[0]] = player_information
-    
-    return player_dic
+        name, height, experience, guardian_names = player
+        one_player_dict = {"name" : name,
+                           "height" : height, 
+	                       "experience" : experience,
+		                   "guardian_names" : guardian_names}
+        list_player_dic.append(one_player_dict)
+
+    return list_player_dic
 
 
 # sort players into three teams
