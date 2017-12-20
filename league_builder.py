571d1934b403ea76feeb69749b1a1dc250ea84fd
neex-io <neex.io@protonmail.com>
2017-12-20 11:41:26 -0800

    Read csv data and save to a dictionary

--- league_builder.py
+++ league_builder.py
@@ -1,4 +1,30 @@
-# import CSV data
+import csv
+
+# import CSV data and save to a dictionary
+
+def csv_to_dictionary(csv_file):
+
+  with open(csv_file, newline='') as players_csv:
+    player_reader = csv.reader(players_csv)
+    players_list = list(player_reader) # list of lists
+    players_list.pop(0) # remove description line 
+    
+    '''players_list conatins each player is a list with this information order:
+    Name = player[0]
+    Height (in inches) = player[1]
+    Experience = player[2]
+    Guardian name(s) = player[3]'''
+  
+    # create a dicitionary of players
+    # the script assumes that there are no players with the same name
+    player_dic = {}
+    for player in players_list:
+      player_information = [player[1], player[2], player[3]] # value for key:value pair of player data
+      player_dic[player[0]] = player_information
+    
+    return player_dic
+
+
 # sort players into three teams
 # save teams to a text file
 
@@ -17,5 +43,7 @@
 
 
 
+
+
 if __name__ == "__main__":
-    pass
+    print(csv_to_dictionary("soccer_players.csv"))
